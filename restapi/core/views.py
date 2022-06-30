6bbc7521019a2116bbc6a0ecf50b6691b2ba1943
Dhruv Soneji <95010022+dhruv5599@users.noreply.github.com>
2022-06-30 19:07:16 +0530

    Add files via upload

---
+++ restapi/core/views.py
@@ -0,0 +1,58 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# third party imports
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+
+from .serializers import PostSerializer
+from .models import Post
+
+
+class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        qs = Post.objects.all()
+        post = qs.first()
+        # serializer = PostSerializer(qs, many=True)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+class PostView(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
